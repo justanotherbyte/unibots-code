6bf525406cd7a27db1ad72a783138d6097bbf49a
Viswa Marepalli <reachvishm@gmail.com>
2026-02-05 18:35:41 +0000

    hey

--- comms.py
+++ comms.py
@@ -14,6 +14,9 @@ def pad_value(value: float) -> str:
 def send_esp32(data: bytes, opcode: int = 0):
   bus.write_i2c_block_data(SLAVE_ADDRESS, opcode, list(data))
 
+def to_b(n: int):
+  return n.to_bytes(byteorder="big", signed=True)
+
 def send_packet(
     left_motor_speed: int,
     right_motor_speed: int,
@@ -21,7 +24,7 @@ def send_packet(
 ):
   send_esp32(left_motor_speed.to_bytes(length=1, byteorder="big", signed=True), opcode=ord('L'))
 
-  bus.write_i2c_block_data(SLAVE_ADDRESS, ord('S'), [*bytes(32), *bytes(16)])
+  bus.write_i2c_block_data(SLAVE_ADDRESS, ord('S'), [*to_b(64), *to_b(16)])
   # send_esp32("R".encode("ascii"))
   # send_esp32(right_motor_speed.to_bytes(length=1, byteorder="big", signed=True))
 
